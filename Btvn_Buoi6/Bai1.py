3d348beb86796a40e4bc9bbab56234df424c1634
Dang2k5 <dangdao2k5@gmail.com>
2024-08-18 00:54:48 +0700

    add btvn_buoi6

---
+++ Btvn_Buoi6/Bai1.py
@@ -0,0 +1,4 @@
+expression = lambda a, b: max(a,b)
+a = input("Nhap a: ")
+b = input("Nhap b: ")
+print(expression(a, b))
---
+++ Btvn_Buoi6/Bai5.py
@@ -0,0 +1,13 @@
+
+def format_phone_number(number_phone):
+    s = ""
+    for i in number_phone: 
+        if i.isdigit() == True:
+            s = s + i
+    if s[0] != "0" or len(s) != 10 :
+        return "Invalid phone number"
+    return s
+l = [". ? 0123456789"," 02319231", ". ? 0923123145", "8421301203", "absc0123145452ds"]
+for i in l:
+    chuanhoa = format_phone_number(i)
+    print(f"Sđt:{i} --> Chuan hoa:{chuanhoa}")
